63ef1a7bdbb5c387f66aef6aa571868e56663d4d
artvine <74613662+artvinee@users.noreply.github.com>
2024-12-22 22:18:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,12 +5,5 @@ import (
 )
 
 func main() {
-	// expr := "((2 * 9) - 3) - 5 / 2"
-	// result, err := core.CalculateExpression(expr)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// } else {
-	// 	fmt.Println("Result:", result)
-	// }
 	core.StartServer()
 }
